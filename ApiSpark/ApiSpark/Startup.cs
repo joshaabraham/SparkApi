14f8e5a82b8e59c60a5f87ee6d6f1b91d174a89b
Remi Duplan <duplan.remi@gmail.com>
2020-04-28 21:58:45 -0400

    ajout token class et override pour surcharge d apres :
    http://www.dotnetawesome.com/2016/09/token-based-authentication-in-webapi.html

---
+++ ApiSpark/ApiSpark/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ApiSpark.Startup))]
+
+namespace ApiSpark
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Pour plus d'informations sur la configuration de votre application, visitez https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //enable cors origin requests
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var myProvider = new MyAuthorizationServerProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
