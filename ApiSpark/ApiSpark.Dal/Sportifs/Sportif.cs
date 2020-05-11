79ebac165da991a7cdac3fed1d2db3e491ed176d
Remi Duplan <duplan.remi@gmail.com>
2020-05-11 00:16:30 -0400

    heritage fait (generation table a faire)

--- ApiSpark/ApiSpark.Dal/Sportifs/Sportif.cs
+++ ApiSpark/ApiSpark.Dal/Sportifs/Sportif.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApiSpark.Dal.LogInfo;
 
 namespace ApiSpark.Dal.Sportifs
 {
-    public class Sportif
+    public class Sportif : User
     {
     }
 }
